3eaded987536efb1980cff19ab3a308bb7bb9b11
oligsei <oligsei.reznikov@gmail.com>
2017-04-12 18:37:48 +0300

    Implemented record storage using HashSet

--- src/main/java/javaschool/app/PhoneBook.java
+++ src/main/java/javaschool/app/PhoneBook.java
@@ -7,14 +7,12 @@ import asg.cliche.ShellFactory;
 import javaschool.app.personfactory.PersonFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PhoneBook implements ShellDependent {
 
-    private List<Record> list = new ArrayList<>();
+    private Map<Integer, Record> map = new HashMap<>();
     private Shell theShell;
 
     @Override
@@ -24,33 +22,32 @@ public class PhoneBook implements ShellDependent {
 
     @Command(description = "Add a new person")
     public void addPerson(String name) {
-        list.add(new Person(name));
+        add(new Person(name));
     }
 
     @Command(description = "Add a new note")
     public void addNote(String name) {
-        list.add(new Note(name));
+        add(new Note(name));
     }
 
     @Command(description = "Add a new reminder")
     public void addReminder(String name) {
-        list.add(new Reminder(name));
+        add(new Reminder(name));
     }
 
     @Command(description = "Edit a record by id")
     public void edit(Integer id) throws IOException {
-        Optional<Record> result = lookup(id);
-        if (result.isPresent()) {
-            Record record = result.get();
-            ShellFactory.createSubshell(record.getName(), theShell, "Editing " + record.getName(), record).commandLoop();
-        } else {
+        Record record = lookup(id);
+        if (record == null) {
             System.out.printf("Record with id \"%d\" not found.\n", id);
+        } else {
+            ShellFactory.createSubshell(record.getName(), theShell, "Editing " + record.getName(), record).commandLoop();
         }
     }
 
     @Command
-    public List<Record> list() {
-        return list;
+    public Collection<Record> list() {
+        return map.values();
     }
 
     @Command
@@ -58,15 +55,15 @@ public class PhoneBook implements ShellDependent {
         type = type.toLowerCase();
 
         if (type.equals("note")) {
-            return list.stream().filter(record -> record instanceof Note).collect(Collectors.toList());
+            return map.values().stream().filter(record -> record instanceof Note).collect(Collectors.toList());
         }
 
         if (type.equals("person")) {
-            return list.stream().filter(record -> record instanceof Person).collect(Collectors.toList());
+            return map.values().stream().filter(record -> record instanceof Person).collect(Collectors.toList());
         }
 
         if (type.equals("reminder")) {
-            return list.stream().filter(record -> record instanceof Reminder).collect(Collectors.toList());
+            return map.values().stream().filter(record -> record instanceof Reminder).collect(Collectors.toList());
         }
 
         return new ArrayList<>();
@@ -83,7 +80,7 @@ public class PhoneBook implements ShellDependent {
 
     @Command
     public void clear() {
-        list.clear();
+        map.clear();
     }
 
     @Command
@@ -93,17 +90,22 @@ public class PhoneBook implements ShellDependent {
 
     @Command(description = "Search in records")
     public List<Record> search(String criteria) {
-        return list.stream().filter((r) -> r.contains(criteria.toLowerCase())).collect(Collectors.toList());
+        return map.values().stream().filter((r) -> r.contains(criteria.toLowerCase())).collect(Collectors.toList());
     }
 
     @Command
     public void generate(Integer count) {
         List<Person> result = PersonFactory.generate(count, PersonFactory.API_RANDOM_USER);
-        list.addAll(result);
+        result.forEach(this::add);
         System.out.format("Created %s new users\n", result.size());
     }
 
-    private Optional<Record> lookup(Integer id) {
-        return list.stream().filter((r) -> r.getId().equals(id)).findFirst();
+    private Record lookup(Integer id) {
+        return map.get(id);
+//        return map.values().stream().filter((r) -> r.getId().equals(id)).findFirst();
+    }
+
+    private void add(Record r) {
+        map.put(r.getId(), r);
     }
 }
